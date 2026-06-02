52f574183c199ffcc14bec77f84eaf6afb3db9fb
Jose Javier Vegas <65979544+vegasjj@users.noreply.github.com>
2026-06-02 16:18:08 +0000

    Add spoke virtual network and test vm1

--- main.tf
+++ main.tf
@@ -65,3 +65,78 @@ resource "azurerm_firewall" "az-fw" {
     subnet_id            = local.subnet_ids["AzureFirewallManagementSubnet"]
   }
 }
+
+resource "azurerm_network_security_group" "spk-vnet-nsg" {
+  name                = var.spoke_vnet_nsg_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  # security_rule {
+  #   name                       = "AllowSSH"
+  #   priority                   = 1001
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "22"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = "*"
+  # }
+}
+
+resource "azurerm_virtual_network" "spk-vnet" {
+  name                = var.spoke_vnet_name
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  subnet {
+      name               = "internal"
+      address_prefixes   = ["10.1.3.0/24"]
+      security_group     = azurerm_network_security_group.spk-vnet-nsg.id
+      default_outbound_access_enabled = false
+  }
+}
+
+locals {
+  spoke_subnet_ids = { for sn in azurerm_virtual_network.spk-vnet.subnet : sn.name => sn.id }
+}
+
+resource "azurerm_network_interface" "vm1_nic" {
+  name                = var.vm1_nic_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = local.spoke_subnet_ids["internal"]
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = var.vm1_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s" # Cost-effective for testing
+  admin_username      = "cloud_user"
+  
+  network_interface_ids = [azurerm_network_interface.vm1_nic.id]
+
+  admin_ssh_key {
+    username   = "cloud_user"
+    public_key = file("~/.ssh/id_ed25519.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "resource_group_name" {
   description = "Name of the Azure Resource Group"
-  default     = "1-341af44f-playground-sandbox"
+  default     = "1-cf12a91a-playground-sandbox"
   type        = string
 }
 
 variable "location" {
   description = "Azure region to deploy resources"
-  default     = "westus"
+  default     = "eastus"
   type        = string
 }
 
@@ -27,3 +27,33 @@ variable "azure_firewall_vnet_name" {
    default     = "test-az-fw-vnet"
    type        = string
 }
+
+variable "spoke_vnet_name" {
+   description = "Name of the Spoke Vnet"
+   default     = "test-spk-vnet"
+   type        = string
+}
+
+variable "spoke_vnet_nsg_name" {
+   description = "Name of the Spoke Vnet NSG"
+   default     = "test-spk-vnet-nsg"
+   type        = string
+}
+
+variable "vm1_name" {
+   description = "Name of test vm1"
+   default     = "vm1"
+   type        = string
+}
+
+variable "vm1_nic_name" {
+   description = "Name of the nic for test vm1"
+   default     = "vm1-nic"
+   type        = string
+}
+
+# variable "vm1_ssh_public_key" {
+#    description = "SSH public key value for test vm1"
+#    type        = string
+#    sensitive   = true
+# }
